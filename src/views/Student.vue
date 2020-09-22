f0904ea7c4282b9855ffcd1049a60f64931488e3
USCANGA <macarioluisangel006@gmail.com>
2020-09-21 21:12:11 -0500

    Se añadió la informacion completa del alumno solo para visualizar

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -2,10 +2,269 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <h2>
-          Bienvenido(a):&nbsp;{{user.nombre}} {{user.apellidoPaterno}} {{user.apellidoMaterno}}
-        </h2>
+        <h2 class="text-center mb-4">UNIVERSIDAD TECNOLOGICA DEL SURESTE DE VERACRUZ</h2>
       </v-col>
+
+<!-- Aqui empieza el container con los datos del alumno-->
+    <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-container>
+                <v-alert
+                    text
+                    dense
+                    color="info"
+                    icon="mdi-information"
+                    border="left"
+                  >La informacion en pantalla es de caracter sensible, no compartas tu usuario y contraseña.</v-alert>
+                <h2 class="mb-5">Datos personales</h2>
+                <v-row>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      disabled
+                      outlined
+                      color="green"
+                      label="Nombre(s)"
+                      placeholder="Ingresa tu nombre(s)"
+                      v-model="user.nombre"
+                      dense
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      disabled
+                      color="green"
+                      label="Apellido paterno"
+                      placeholder="Ingresa tu apellido paterno"
+                      v-model="user.apellidoPaterno"
+                      dense
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      disabled
+                      outlined
+                      color="green"
+                      label="Correo electrónico"
+                      placeholder="Ingresa tu correo electrónico"
+                      dense
+                      v-model="user.correoElectronico"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      disabled
+                      outlined
+                      color="green"
+                      label="Apellido materno"
+                      placeholder="Ingresa tu apellido materno"
+                      v-model="user.apellidoMaterno"
+                      dense
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      disabled
+                      outlined
+                      dense
+                      color="green"
+                      v-model="user.fechaNacimiento"
+                      label="Fecha de nacimiento"
+                      placeholder="Ingresa tu fecha de nacimiento"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      disabled
+                      color="green"
+                      label="Edad"
+                      placeholder="Ingresa tu edad"
+                      type="number"
+                      min="0"
+                      max="99"
+                      v-model="user.edad"
+                      dense
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      disabled
+                      color="green"
+                      label="Sexo"
+                      placeholder="Ingresa tu sexo"
+                      dense
+                      v-model="user.sexo"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      disabled
+                      color="green"
+                      label="Número seguro social"
+                      placeholder="Ingresa número de seguridad social"
+                      type="number"
+                      min="0"
+                      max="99"
+                      dense
+                      v-model="user.numeroSeguroSocial"
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      disabled
+                      color="green"
+                      label="Padre o tutor"
+                      placeholder="Ingresa el nombre de tu padre o tutor"
+                      dense
+                      v-model="user.tutor"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-container>
+                <h2 class="mb-5">Dirección</h2>
+                <v-row>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      color="green"
+                      label="Calle"
+                      placeholder="Ingresa tu calle"
+                      dense
+                      v-model="user.direccion.calle"
+                      disabled
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4"  class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      color="green"
+                      label="Número exterior"
+                      placeholder="Ingresa el número exterior"
+                      type="number"
+                      min="0"
+                      max="99999"
+                      dense
+                      v-model="user.direccion.numeroExterior"
+                      disabled
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4"  class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      color="green"
+                      label="Número interior"
+                      placeholder="Ingresa número interior"
+                      type="number"
+                      min="0"
+                      max="99999"
+                      dense
+                      v-model="user.direccion.numeroInterior"
+                      disabled
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4"  class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      color="green"
+                      label="Colonia"
+                      placeholder="Ingresa tu colonia"
+                      dense
+                      v-model="user.direccion.colonia"
+                      disabled
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4"  class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      color="green"
+                      label="Código postal"
+                      placeholder="Ingresa tu código postal"
+                      type="number"
+                      min="0"
+                      max="99999"
+                      dense
+                      v-model="user.direccion.codigoPostal"
+                      disabled
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4"  class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      color="green"
+                      label="Estado"
+                      placeholder="Ingresa tu estado"
+                      dense
+                      v-model="user.direccion.estado"
+                      disabled
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      color="green"
+                      label="Ciudad"
+                      placeholder="Ingresa tu ciudad"
+                      dense
+                      v-model="user.direccion.ciudad"
+                      disabled
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-container>
+                <h2 class="mb-5">Datos escolares</h2>
+                <v-row>
+                  <v-col cols="12" md="6" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      disabled
+                      color="green"
+                      label="Escuela de procedencia"
+                      placeholder="Ingresa tu escuela de procedencia"
+                      dense
+                      v-model="user.escuelaProcedencia"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="6" class="ma-0 pt-0 pb-0">
+                    <v-text-field
+                      outlined
+                      disabled
+                      color="green"
+                      label="Carrera"
+                      placeholder="Selecciona la carrera a la que deseas inscribirte"
+                      dense
+                      v-model="user.carrera"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container> <!-- Aqui termina el container con los datos del alumno-->
+
       <v-col cols="4" v-for="(document, index) in user.documents" :key="document.name">
         <v-card>
           <v-card-title>{{document.name}}</v-card-title>
